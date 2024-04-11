63acb8d047d8088bae4c9899e0b6f1baafe25afd
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-11 17:08:52 -0300

    Criação do Projeto Localizaçã

---
+++ src/test/java/io/github/thiago11112000/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thiago11112000.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
